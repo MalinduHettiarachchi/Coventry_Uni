831b619c0c0adc7ff20e97f9569fbd7793dd2165
Malindu Hettiarachchi <mhssc20@gmail.com>
2024-11-05 20:50:49 +0530

    Update App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import '@fortawesome/fontawesome-free/css/all.min.css';
 import './App.css';
 import Home from './Pages/Home/home';
 import Login from './Pages/Login/login';
